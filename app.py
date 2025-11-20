cb9b21355499d23c2d1a85e82686900841de5e91
JEAH-9 <edward_9499@hotmail.com>
2025-11-20 15:21:30 -0600

    grafico de Dispercion

--- app.py
+++ app.py
@@ -9,9 +9,19 @@ if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
         '1. Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Construir Grafico de Dispercion')  # crear un botón
+
+if disp_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('2. Creación de un Grafico de Dispercion para el conjunto de datos de anuncios de venta de coches')
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer")
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
